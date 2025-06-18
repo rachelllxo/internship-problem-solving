bbe12718ad98f97db26556986f09b48e8d6d9467
rachelllxo <rachelveronica309@gmail.com>
2025-06-18 13:34:37 +0530

    Added Day-3

---
+++ Day-3/matrix-rotation_problem1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[100][100], matrix_size;
+
+    cout << "Enter the size of the matrix: ";
+    cin >> matrix_size;
+	int matrix_elements[100][100]; // You were trying to subscript an int by mistake
+
+    cout << "Enter the elements in the matrix:\n";
+    for (int rows = 0; rows < matrix_size; rows++) {
+        for (int columns = 0; columns < matrix_size; columns++) {
+            cin >> matrix_elements[rows][columns];
+        }
+    }
+
+    // Transpose the matrix
+    for (int rows = 0; rows < matrix_size; rows++) {
+        for (int columns = rows + 1; columns < matrix_size; columns++) {
+            int transpose_matrix = matrix_elements[rows][columns];
+            matrix_elements[rows][columns] = matrix_elements[columns][rows];
+            matrix_elements[columns][rows] = transpose_matrix;
+        }
+    }
+
+    // Reverse each row
+    for (int rows = 0; rows < matrix_size; rows++) {
+        for (int columns = 0; columns < matrix_size / 2; columns++) {
+            int rotated_matrix = matrix_elements[rows][columns];
+            matrix_elements[rows][columns] = matrix_elements[rows][matrix_size - 1 - columns];
+            matrix_elements[rows][matrix_size - 1 - columns] = rotated_matrix;
+        }
+    }
+
+    // Print the rotated matrix
+    cout << "The rotated matrix:\n";
+    for (int rows = 0; rows < matrix_size; rows++) {
+        for (int columns = 0; columns < matrix_size; columns++) {
+            cout << matrix_elements[rows][columns] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+};
