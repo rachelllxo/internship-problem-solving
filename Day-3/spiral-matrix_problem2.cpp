ba7a4071929dabc38db0da21403e4d0253f23312
rachelllxo <rachelveronica309@gmail.com>
2025-06-19 09:38:10 +0530

    Added Comments

--- Day-3/spiral-matrix_problem2.cpp
+++ Day-3/spiral-matrix_problem2.cpp
@@ -16,32 +16,32 @@ int main() {
         for (int j = 0; j < columns; j++)
             cin >> matrix[i][j];
 
- 
+    //initialising to make shifting easier 
     int top = 0, bottom = rows - 1, left = 0, right = columns - 1;
 
-    while (top <= bottom && left <= right) {
+    while (top <= bottom && left <= right) { //end at last right element 
         for (int i = left; i <= right; i++)
             spiral[index++] = matrix[top][i];
         top++;
 
-        for (int i = top; i <= bottom; i++)
+        for (int i = top; i <= bottom; i++) // ends at last bottom element 
             spiral[index++] = matrix[i][right];
         right--;
 
         if (top <= bottom) {
-            for (int i = right; i >= left; i--)
+            for (int i = right; i >= left; i--) //ends at last left element 
                 spiral[index++] = matrix[bottom][i];
             bottom--;
         }
 
         if (left <= right) {
-            for (int i = bottom; i >= top; i--)
+            for (int i = bottom; i >= top; i--) //moves top to left 
                 spiral[index++] = matrix[i][left];
             left++;
         }
     }
 
-    int last = spiral[index - 1];
+    int last = spiral[index - 1]; //shifts the last element to the first top left element 
     for (int i = index - 1; i > 0; i--) {
         spiral[i] = spiral[i - 1];
     }
