e48ac3877e442dea454d8cdeb09451079af16681
rachelllxo <rachelveronica309@gmail.com>
2025-06-29 11:13:42 +0530

    Edited

--- Day-10/name.contact_problem1.cpp
+++ Day-10/name.contact_problem1.cpp
@@ -1,50 +1,43 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Structure to store contact details
+// Structure to store contact
 struct Contact {
-    char name[50];         // Character array for name
-    int phone_number;      // Integer for phone number
-    Contact* next;         // Pointer to next contact node
+    string name;        // using string for name
+    int phone_number;   // int for phone number
+    Contact* next;      // pointer to next contact
 };
 
-Contact* head = NULL;      // Start with an empty list
+Contact* head = NULL;  // start with empty list
 
-// Function to add a contact to the end of the list
-void add_contact(char name[], int phone_number) {
+// Function to add a contact to the end
+void add_contact(string name, int phone_number) {
     Contact* new_node = new Contact();
-
-    // Copy name character by character
-    int count = 0;
-    while (name[count] != '\0') {
-        new_node->name[count] = name[count];
-        count++;
-    }
-    new_node->name[count] = '\0';  // Null-terminate the string
-
+    new_node->name = name;
     new_node->phone_number = phone_number;
     new_node->next = NULL;
 
     if (head == NULL) {
-        head = new_node;  // First node
+        head = new_node;  // first contact
     } else {
         Contact* temp = head;
         while (temp->next != NULL) {
-            temp = temp->next;
+            temp = temp->next;  // move to last node
         }
-        temp->next = new_node;  // Link the last node to new one
+        temp->next = new_node;  // link new contact
     }
 }
 
-// Function to display all contacts
+// Function to print all contacts
 void display_contacts() {
     if (head == NULL) {
         cout << "No contacts to display.\n";
         return;
     }
 
-    Contact* temp = head;
     cout << "\nStored Contacts:\n";
+    Contact* temp = head;
     while (temp != NULL) {
         cout << "Name: " << temp->name << ", Phone Number: " << temp->phone_number << endl;
         temp = temp->next;
@@ -52,23 +45,23 @@ void display_contacts() {
 }
 
 int main() {
-    int noOfContacts;
+    int n;
     cout << "How many contacts do you want to store? ";
-    cin >> noOfContacts;
+    cin >> n;
 
-    for (int count = 0; count < noOfContacts; count++) {
-        char name[50];
+    for (int i = 0; i < n; i++) {
+        string name;
         int phone;
 
-        cout << "\nEnter name: " << count + 1 << ": ";
-        cin >> name;  
+        cout << "\nEnter name for contact " << i + 1 << ": ";
+        cin >> name;  // use underscore if there's a space
 
-        cout << "Enter phone number: " << count + 1 << ": ";
+        cout << "Enter phone number for contact " << i + 1 << ": ";
         cin >> phone;
 
         add_contact(name, phone);
     }
 
-    display_contacts();  // Show all saved contacts
+    display_contacts();
     return 0;
 }
