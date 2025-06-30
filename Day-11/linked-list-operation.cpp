2c02dd65daa455525704baea3c4818efb3f344b5
rachelllxo <rachelveronica309@gmail.com>
2025-06-30 21:17:01 +0530

    Edited code

--- Day-11/linked-list-operation.cpp
+++ Day-11/linked-list-operation.cpp
@@ -11,10 +11,11 @@ struct Node {
 Node* head = NULL;
 
 // Insert node at the end
-void insert_node(int value) {
+int insert_node(int value) {
     Node* new_node = new Node();
     new_node->data = value;
     new_node->next = NULL;
+
     if (head == NULL) {
         head = new_node;
     } else {
@@ -24,25 +25,27 @@ void insert_node(int value) {
         }
         temp->next = new_node;
     }
+    return 0;
 }
 
 // Insert node at the beginning
-void insert_at_beginning(int value) {
+int insert_at_beginning(int value) {
     Node* new_node = new Node();
     new_node->data = value;
     new_node->next = head;
     head = new_node;
+    return 0;
 }
 
 // Insert node at a specific position 
-void insert_at_position(int position, int value) {
+int insert_at_position(int position, int value) {
     Node* new_node = new Node();
     new_node->data = value;
 
     if (position == 0) {
         new_node->next = head;
         head = new_node;
-        return;
+        return 0;
     }
 
     Node* temp = head;
@@ -56,25 +59,26 @@ void insert_at_position(int position, int value) {
     if (temp == NULL) {
         cout << "Position out of range" << endl;
         delete new_node;
-        return;
+        return -1;
     }
 
     new_node->next = temp->next;
     temp->next = new_node;
+    return 0;
 }
 
 // Delete node at a specific position 
-void delete_node(int position) {
+int delete_node(int position) {
     if (head == NULL) {
         cout << "List is empty" << endl;
-        return;
+        return -1;
     }
 
     if (position == 0) {
         Node* temp = head;
         head = head->next;
         delete temp;
-        return;
+        return 0;
     }
 
     Node* temp = head;
@@ -87,16 +91,17 @@ void delete_node(int position) {
 
     if (temp->next == NULL) {
         cout << "Position out of range" << endl;
-        return;
+        return -1;
     }
 
     Node* node_to_delete = temp->next;
     temp->next = node_to_delete->next;
     delete node_to_delete;
+    return 0;
 }
 
 // Reverse the linked list
-void reverse_list() {
+int reverse_list() {
     Node* prev = NULL;
     Node* curr = head;
     Node* next = NULL;
@@ -108,20 +113,22 @@ void reverse_list() {
         curr = next;
     }
     head = prev;
+    return 0;
 }
 
 // Display the list 
-void display_list() {
+int display_list() {
     Node* temp = head;
     while (temp != NULL) {
         cout << temp->data << " ";
         temp = temp->next;
     }
     cout << endl;
+    return 0;
 }
 
 // Menu-driven interface without using break
-void menu() {
+int menu() {
     int choice = 0;
 
     while (choice != 5) {
@@ -175,11 +182,14 @@ void menu() {
             cout << "Invalid choice.\n";
         }
     }
+    return 0;
 }
+
+// Main function
 int main() {
     int noOfnodes, val;
     cout << "Enter the number of nodes: ";
-    cin >>  noOfnodes;
+    cin >> noOfnodes;
     cout << "Enter the linked list: ";
     for (int counter = 0; counter < noOfnodes; counter++) {
         cin >> val;
